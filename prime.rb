6d3e15ccbef3b19090fb8120c3d12183976e5b05
Jackie Ha <jha2@wellesley.edu>
2018-08-26 16:49:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,8 @@
 # Add  code here!
-def prime? (integer)
-  if integer < 2 
-    return false 
-  end 
-  (2..n-1).to_a.each do |x|
-    if (n % x) == 0 
+def prime?(integer)
+  return false if integer < 2 
+  (2..integer - 1).each do |x|
+    if (integer % x) == 0 
       return false 
     end 
   end 
