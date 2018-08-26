b803366d76cb4d841eec280a67b69c03d1cba163
Jackie Ha <jha2@wellesley.edu>
2018-08-26 16:46:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
 # Add  code here!
 def prime? (integer)
-  if integer < 2 
-    return false 
-  else
-  else
-
+  return false if integer < 2 
+  (2..n-1).each do |x|
+    if (n%x) == 0 
+      return false
+    end
+  end
+  true 
 end
